6cea9a5f98d5485663d770f7031a55accf728498
S.Abhishek <137529295+Abhi-9126@users.noreply.github.com>
2024-03-16 08:33:03 +0530

    Delete assignment3 directory

--- assignment3/in.js
+++
@@ -1,9 +0,0 @@
-function cal(){
-    var amount=document.getElementById("amount").value;
-    var interest=document.getElementById("interest").value;
-    var time=document.getElementById("time").value;
-    var totalin=((amount*interest)/100)*time;
-    var totalamount=parseInt(amount)+totalin;
-    document.getElementById("Interest-re").innerHTML="Total Interest= "+totalin;
-    document.getElementById("amount-re").innerHTML="Total amount to repayed= "+totalamount;
-}
